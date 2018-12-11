2f83a1e6844502b96311844a4934cdc4b29af3e4
huangjixin <517714860@qq.com>
2018-12-11 11:44:36 +0800

    添加安全配置

--- micro-cloud-root/micro-cloud-admin-member/src/main/java/com/zhiyesoft/cloud/MicroCloudAdminMemberApplication.java
+++ micro-cloud-root/micro-cloud-admin-member/src/main/java/com/zhiyesoft/cloud/MicroCloudAdminMemberApplication.java
@@ -2,8 +2,17 @@ package com.zhiyesoft.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+@MapperScan("com.zhiyesoft.cloud.modules.mem.mapper")
 public class MicroCloudAdminMemberApplication {
 
 	public static void main(String[] args) {
---
+++ micro-cloud-root/micro-cloud-admin-member/src/main/java/com/zhiyesoft/cloud/config/DateConfig.java
@@ -0,0 +1,31 @@
+package com.zhiyesoft.cloud.config;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class DateConfig {
+
+	@Bean
+    public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
+    	MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+    	//设置日期格式
+    	ObjectMapper objectMapper = new ObjectMapper();
+    	SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    	objectMapper.setDateFormat(smt);
+    	mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+    	//设置中文编码格式
+    	List<MediaType> list = new ArrayList<MediaType>();
+    	list.add(MediaType.APPLICATION_JSON_UTF8);
+    	mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+    	return mappingJackson2HttpMessageConverter;
+    }
+}
