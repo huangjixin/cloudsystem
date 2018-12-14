84a89f67043153be52571d7d3434c0add26ca59b
huangjixin <517714860@qq.com>
2018-12-14 09:42:17 +0800

    添加日志打印

--- micro-cloud-root/micro-cloud-admin-system-service/src/main/java/com/zhiyesoft/cloud/modules/system/service/impl/UserDetailService.java
+++ micro-cloud-root/micro-cloud-admin-system-service/src/main/java/com/zhiyesoft/cloud/modules/system/service/impl/UserDetailService.java
@@ -2,6 +2,8 @@ package com.zhiyesoft.cloud.modules.system.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,17 +18,24 @@ import com.zhiyesoft.cloud.modules.system.vo.UserVo;
 @Service
 public class UserDetailService implements UserDetailsService {
 
+	////////////////////////////////////////////////////////
+	///// 变量区域
+	////////////////////////////////////////////////////////
+
+	private static Logger logger = LoggerFactory.getLogger(UserDetailService.class);
 	@Autowired
 	private IUserService userService;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		logger.info("Security-service申请授权token开始，传进来的username是："+username);
 		/*UserCriteria userCriteria = new UserCriteria();
 		userCriteria.createCriteria().andLoginNameEqualTo(username);
 		userCriteria.or(userCriteria.createCriteria().andMobileEqualTo(username));
 		List<User> list = this.userService.selectByExample(userCriteria);
 		UserDetails userDetails = ((list!=null && list.size()>0?list.get(0):null));*/
 		UserVo userVo = this.userService.selectByUsername(username);
+		logger.info("this.userService.selectByUsername(username)调用的结果是："+(userVo!=null?userVo.toString():"null"));
 		return userVo;
 	}
 
