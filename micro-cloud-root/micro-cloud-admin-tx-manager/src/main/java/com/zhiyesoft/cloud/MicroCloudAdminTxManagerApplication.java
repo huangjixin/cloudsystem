157f27f8895f22a83fc03b74627e6e9ab02e1575
huangjixin <517714860@qq.com>
2018-12-18 10:41:29 +0800

    创建分布式项目

---
+++ micro-cloud-root/micro-cloud-admin-tx-manager/src/main/java/com/zhiyesoft/cloud/MicroCloudAdminTxManagerApplication.java
@@ -0,0 +1,14 @@
+package com.zhiyesoft.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCloudAdminTxManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCloudAdminTxManagerApplication.class, args);
+	}
+
+}
+
