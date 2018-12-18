4e1ee61e761ac396c1271174060a562f252068c8
huangjixin <517714860@qq.com>
2018-12-18 10:59:46 +0800

    加上分布式项目

--- micro-cloud-root/micro-cloud-admin-tx-manager/src/main/java/com/zhiyesoft/cloud/MicroCloudAdminTxManagerApplication.java
+++ micro-cloud-root/micro-cloud-admin-tx-manager/src/main/java/com/zhiyesoft/cloud/MicroCloudAdminTxManagerApplication.java
@@ -2,8 +2,10 @@ package com.zhiyesoft.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroCloudAdminTxManagerApplication {
 
 	public static void main(String[] args) {
