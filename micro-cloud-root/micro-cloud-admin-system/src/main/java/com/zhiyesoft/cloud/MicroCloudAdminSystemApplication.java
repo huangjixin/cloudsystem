f9708b9e6035e79685768951feab31e5378239de
huangjixin <517714860@qq.com>
2019-01-15 14:28:05 +0800

    添加事务注解

--- micro-cloud-root/micro-cloud-admin-system/src/main/java/com/zhiyesoft/cloud/MicroCloudAdminSystemApplication.java
+++ micro-cloud-root/micro-cloud-admin-system/src/main/java/com/zhiyesoft/cloud/MicroCloudAdminSystemApplication.java
@@ -9,13 +9,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.alibaba.druid.pool.DruidDataSource;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
-
+@EnableTransactionManagement
 @EnableFeignClients
 @SpringBootApplication
 @EnableEurekaClient
